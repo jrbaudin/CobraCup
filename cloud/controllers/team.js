ad53521bc4873342f311cb2dbeabf78a84011e5c
Joel Baudin <joel.baudin@tieto.com>
2014-11-11 13:45:17 +0100

    Fixed a prod and dev app for CC
    Also worked on the more advanced statistics functions and so on

--- cloud/controllers/team.js
+++ cloud/controllers/team.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
-var Mailgun = require('mailgun');
-Mailgun.initialize('mg.cobracup.se', 'key-bc14dd14e4c28a20da1bdbc5f5f1223a');
+//var Mailgun = require('mailgun');
+//Mailgun.initialize('mg.cobracup.se', 'key-bc14dd14e4c28a20da1bdbc5f5f1223a');
 
 exports.getTeam = function(req, res) {
   console.log("Trying to get the team..");
@@ -10,8 +10,49 @@ exports.getTeam = function(req, res) {
   teamQuery.include('nhlTeam');
   teamQuery.find().then(function(theTeam) {
     if (theTeam) {
-      res.render('team', { 
-        teamObj: theTeam
+      var innerTeamQuery = new Parse.Query(Team);
+      innerTeamQuery.equalTo('objectId', theTeam[0].id);
+
+      var TeamStat = Parse.Object.extend('TeamStat');
+      var teamStatQuery = new Parse.Query(TeamStat);
+      teamStatQuery.matchesQuery('team', innerTeamQuery);
+      teamStatQuery.find().then(function(stat) {
+        if (stat) {
+          var Standings = Parse.Object.extend('Standings');
+          var standingsQuery = new Parse.Query(Standings);
+          standingsQuery.descending('points');
+          standingsQuery.include(["team.nhlTeam"]);
+          standingsQuery.limit(3)
+          standingsQuery.find().then(function(standings) {
+            if (standings) {
+              res.render('team', { 
+                teamObj: theTeam,
+                statistic: stat,
+                standings: standings
+              });
+            } else {
+              res.render('team', { 
+                teamObj: theTeam,
+                statistic: stat
+              });
+            }
+          },
+          function(error) {
+            console.error('Error when trying to get team standings');
+            console.error(error);
+            res.render('hub', {flashError: 'Problem när det önskade laget skulle hämtas.'});
+          });
+        } else {
+          console.log("couldn't get the stat.. ");
+          res.render('team', { 
+            teamObj: theTeam
+          });
+        }
+      },
+      function(error) {
+        console.error('Error when trying to get team stats');
+        console.error(error);
+        res.render('hub', {flashError: 'Problem när det önskade laget skulle hämtas.'});
       });
     } else {
       res.render('hub', {
