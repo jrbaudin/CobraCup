ff89bae1181fc5d08d3b87db336217875f7dc69f
Joel Baudin <joel.baudin88@gmail.com>
2015-10-02 17:24:03 +0200

    working with migrating player objects

--- cloud/main.js
+++ cloud/main.js
@@ -70,3 +70,75 @@ Parse.Cloud.define("cleanPlayerStats", function(request, response) {
 	  	// Every object is updated.
 	});
 });
+
+Parse.Cloud.define("migratePlayers", function(request, response) {
+	var Team = Parse.Object.extend('Team');
+	var teamQuery = new Parse.Query(Team);
+	teamQuery.find().then(function(results) {
+		var promise = Parse.Promise.as();
+	  	_.each(results, function(result) {
+		  	// For each item, extend the promise with a function to save it.
+
+		  	var captain = {"name":result.get("captain_name"), 
+		  					"email":result.get("captain_email"), 
+		  					"telephone":result.get("captain_telephone"), 
+		  					"gamertag":result.get("captain_gamertag"),
+		  					"playerid":result.get("captain_id")
+		  				};
+
+		  	var lieutenant = {"name":result.get("lieutenant_name"), 
+		  					"email":result.get("lieutenant_email"), 
+		  					"telephone":result.get("lieutenant_telephone"), 
+		  					"gamertag":result.get("lieutenant_gamertag"),
+		  					"playerid":result.get("lieutenant_id")
+		  				};	
+
+		  	console.log("Captain =  " + JSON.stringify(captain));
+		  	console.log("Lieutenant =  " + JSON.stringify(lieutenant));
+
+  		    promise = promise.then(function() {
+  		    	Parse.Cloud.run('createPlayer', captain, {
+  		    	  success: function(result) {
+  		    	  	console.log(result)
+  		    	  },
+  		    	  error: function(error) {
+  		    	  	console.log(error)
+  		    	  }
+  		    	});
+
+  		    	Parse.Cloud.run('createPlayer', lieutenant, {
+  		    	  success: function(result) {
+  		    	  	console.log(result)
+  		    	  },
+  		    	  error: function(error) {
+  		    	  	console.log(error)
+  		    	  }
+  		    	});
+
+  		      	return true;
+  	    	});
+		  	
+	  	});
+
+	  	return promise;
+
+	}).then(function() {
+	    response.success("Players are migrated");
+	  	// Every object is updated.
+	});
+});
+
+Parse.Cloud.define("createPlayer", function(request, response) {
+	Parse.Cloud.useMasterKey();
+	var Player = Parse.Object.extend("Player");
+  	var player = new Player();
+
+  	player.set('name', "test");
+  
+
+  	player.save().then(function(result) {
+  		response.success("Saved the Player with objectId = " + result.id);
+  	}, function(error) {
+    	response.error('Error saving Player, try again. Msg: ' + error.message);
+  	});
+});
