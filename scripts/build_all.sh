7fb41f2d3ca451d8cbdcc832b7e84bd7bedf8f48
Joel Baudin <joel.baudin88@gmail.com>
2015-10-27 14:32:05 +0100

    modified build script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
-ls -l public/
-PARSE_CONFIG_DIR="config"
-mkdir -p "$PARSE_CONFIG_DIR"
-cp -rf public public
-ls -lf public
-go run scripts/gen_travis_parse_keys.go > "$PARSE_CONFIG_DIR/global.json"
-parse deploy
+PARSE_CONFIG_DIR="parse_app/config"
+mkdir -p "$PARSE_CONFIG_DIR" # git does not allow empty dirs to be checked in.
+cp -rf public parse_app/public # copy generated website to parse_app location
+go run scripts/gen_travis_parse_keys.go > "$PARSE_CONFIG_DIR/global.json" #gen keys.
+cd parse_app
+parse deploy #deploy to parse
